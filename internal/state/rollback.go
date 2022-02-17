e9ba4d7d32599d2c52ad21e13ac66718c885b518
HuangYi <huang@crypto.com>
2022-02-17 17:50:58 +0800

    fix app hash in state rollback

--- internal/state/rollback.go
+++ internal/state/rollback.go
@@ -37,6 +37,7 @@ func Rollback(bs BlockStore, ss Store) (int64, []byte, error) {
 
 	// state store height is equal to blockstore height. We're good to proceed with rolling back state
 	rollbackHeight := invalidState.LastBlockHeight - 1
+	currentBlock := bs.LoadBlockMeta(invalidState.LastBlockHeight)
 	rollbackBlock := bs.LoadBlockMeta(rollbackHeight)
 	if rollbackBlock == nil {
 		return -1, nil, fmt.Errorf("block at height %d not found", rollbackHeight)
@@ -90,7 +91,7 @@ func Rollback(bs BlockStore, ss Store) (int64, []byte, error) {
 		LastHeightConsensusParamsChanged: paramsChangeHeight,
 
 		LastResultsHash: rollbackBlock.Header.LastResultsHash,
-		AppHash:         rollbackBlock.Header.AppHash,
+		AppHash:         currentBlock.Header.AppHash,
 	}
 
 	// persist the new state. This overrides the invalid one. NOTE: this will also
