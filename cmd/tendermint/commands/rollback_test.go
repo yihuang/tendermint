5dce1877713819379c1bdf0cffbab993dbdc8473
HuangYi <huang@crypto.com>
2022-03-10 12:32:52 +0800

    rollback command doesn't rollback state if block store has one more block
    it's not very useful under the app hash mismatch scenario.
    this patch will rollback the block store in such case.
    
    changelog
    
    fix pr suggestion
    
    unit test
    
    update unit test, the single node chain is expected to halt
    
    temp

--- cmd/tendermint/commands/rollback_test.go
+++ cmd/tendermint/commands/rollback_test.go
@@ -2,12 +2,15 @@ package commands_test
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/tendermint/tendermint/cmd/tendermint/commands"
+	"github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/log"
 	"github.com/tendermint/tendermint/rpc/client/local"
 	rpctest "github.com/tendermint/tendermint/rpc/test"
@@ -16,47 +19,84 @@ import (
 
 func TestRollbackIntegration(t *testing.T) {
 	var height int64
-	dir := t.TempDir()
+	dir1 := t.TempDir()
+	dir2 := t.TempDir()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	cfg, err := rpctest.CreateConfig(t, t.Name())
+	cfg1, err := rpctest.CreateConfig(t, t.Name())
 	require.NoError(t, err)
-	cfg.BaseConfig.DBBackend = "goleveldb"
+	cfg1.BaseConfig.DBBackend = "goleveldb"
 
-	app, err := e2e.NewApplication(e2e.DefaultConfig(dir))
+	cfg2, err := rpctest.CreateConfig(t, t.Name()+"Full")
+	cfg2.Mode = config.ModeFull
+	cfg2.BaseConfig.DBBackend = "goleveldb"
+
+	// connect the peers
+	id1, err := cfg1.LoadOrGenNodeKeyID()
+	require.NoError(t, err)
+	id2, err := cfg2.LoadOrGenNodeKeyID()
+	require.NoError(t, err)
+	port1 := strings.Split(cfg1.P2P.ListenAddress, ":")[2]
+	port2 := strings.Split(cfg2.P2P.ListenAddress, ":")[2]
+	cfg1.P2P.PersistentPeers = fmt.Sprintf("tcp://%s@localhost:%s", id2, port2)
+	cfg2.P2P.PersistentPeers = fmt.Sprintf("tcp://%s@localhost:%s", id1, port1)
+	fmt.Println(cfg1.P2P.PersistentPeers, cfg2.P2P.PersistentPeers)
+
+	app1, err := e2e.NewApplication(e2e.DefaultConfig(dir1))
+	require.NoError(t, err)
+	app2, err := e2e.NewApplication(e2e.DefaultConfig(dir2))
 	require.NoError(t, err)
 
 	t.Run("First run", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
+		ctx1, cancel1 := context.WithCancel(ctx)
+		defer cancel1()
+		ctx2, cancel2 := context.WithCancel(ctx)
+		defer cancel2()
+
+		node1, _, err := rpctest.StartTendermint(ctx1, cfg1, app1, rpctest.SuppressStdout)
 		require.NoError(t, err)
-		node, _, err := rpctest.StartTendermint(ctx, cfg, app, rpctest.SuppressStdout)
+		node2, _, err := rpctest.StartTendermint(ctx2, cfg2, app2, rpctest.SuppressStdout)
 		require.NoError(t, err)
-		require.True(t, node.IsRunning())
+
+		require.True(t, node1.IsRunning())
+		require.True(t, node2.IsRunning())
 
 		time.Sleep(3 * time.Second)
-		cancel()
-		node.Wait()
 
-		require.False(t, node.IsRunning())
+		cancel1()
+		node1.Wait()
+		cancel2()
+		node2.Wait()
+
+		require.False(t, node1.IsRunning())
+		require.False(t, node2.IsRunning())
 	})
 	t.Run("Rollback", func(t *testing.T) {
 		time.Sleep(time.Second)
-		require.NoError(t, app.Rollback())
-		height, _, err = commands.RollbackState(cfg)
+		require.NoError(t, app1.Rollback())
+		height, _, err = commands.RollbackState(cfg1)
 		require.NoError(t, err, "%d", height)
+	})
+	t.Run("Rollback again", func(t *testing.T) {
+		// should be able to rollback again.
+		require.NoError(t, app1.Rollback())
+		height2, _, err := commands.RollbackState(cfg1)
+		require.NoError(t, err, "%d", height2)
+		require.Equal(t, height-1, height2)
 	})
 	t.Run("Restart", func(t *testing.T) {
 		require.True(t, height > 0, "%d", height)
 
-		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, 40*time.Second)
 		defer cancel()
-		node2, _, err2 := rpctest.StartTendermint(ctx, cfg, app, rpctest.SuppressStdout)
-		require.NoError(t, err2)
+		_, _, err := rpctest.StartTendermint(ctx, cfg2, app2, rpctest.SuppressStdout)
+		require.NoError(t, err)
+		node1, _, err := rpctest.StartTendermint(ctx, cfg1, app1)
+		require.NoError(t, err)
 
 		logger := log.NewNopLogger()
 
-		client, err := local.New(logger, node2.(local.NodeService))
+		client, err := local.New(logger, node1.(local.NodeService))
 		require.NoError(t, err)
 
 		ticker := time.NewTicker(200 * time.Millisecond)
@@ -68,7 +108,7 @@ func TestRollbackIntegration(t *testing.T) {
 				status, err := client.Status(ctx)
 				require.NoError(t, err)
 
-				if status.SyncInfo.LatestBlockHeight > height {
+				if status.SyncInfo.LatestBlockHeight > height+2 {
 					return
 				}
 			}
